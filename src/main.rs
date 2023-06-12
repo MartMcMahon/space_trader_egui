2015663dc012160289f0805b096d632b5d514da2
Mart <themcman@gmail.com>
2023-06-11 22:10:46 -0500

    gets data

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 use directories::UserDirs;
 use eframe::egui;
+use reqwest::header::{HeaderMap, HeaderValue};
 use reqwest::Client;
 use serde_derive::{Deserialize, Serialize};
 use std::sync::mpsc::{Receiver, Sender};
@@ -26,6 +27,10 @@ struct STApp {
     credits: u32,
     corpo_name: String,
     validation_text: String,
+    main_text: String,
+    error_text: String,
+
+    agent_data: AgentData,
 }
 
 // enum RequestBody {
@@ -73,7 +78,7 @@ fn read_token() -> Result<String, TokenReadError> {
     let user_dirs = UserDirs::new().unwrap();
     let token_file_path = user_dirs.home_dir().join(".space_traders/token");
 
-    let mut file = File::open(token_file_path);
+    let file = File::open(token_file_path);
     match file {
         Ok(mut f) => {
             let mut token = String::new();
@@ -88,17 +93,14 @@ fn read_token() -> Result<String, TokenReadError> {
     }
 }
 
-fn main() {
-    // initilize app object
-    let token = read_token();
-    let main_app = match token {
-        Ok(token) => {
-            println!("{token}");
-            STApp::with_token(token)
-        }
-        Err(_) => STApp::default(),
-    };
+fn write_token(token: String) -> Result<usize, std::io::Error> {
+    let user_dirs = UserDirs::new().unwrap();
+    let token_file_path = user_dirs.home_dir().join(".space_traders/token");
+    let mut file = File::create(token_file_path).expect("creation failed");
+    file.write(token.as_bytes())
+}
 
+fn main() {
     // create runtime
     let rt = Runtime::new().expect("Unable to create Runtime");
     // Enter the runtime so that `tokio::spawn` is available immediately.
@@ -113,6 +115,13 @@ fn main() {
         })
     });
 
+    // initilize app object
+    let token_res = read_token();
+    let main_app = match token_res {
+        Ok(token) => STApp::with_token(token),
+        Err(_) => STApp::default(),
+    };
+
     // Run the GUI in the main thread.
     eframe::run_native(
         "Space Traders",
@@ -125,6 +134,7 @@ impl STApp {
     fn with_token(token: String) -> Self {
         Self {
             token,
+            state: AppState::Main,
             ..Default::default()
         }
     }
@@ -146,6 +156,10 @@ impl Default for STApp {
 
             corpo_name: String::new(),
             validation_text: String::new(),
+            main_text: String::new(),
+            error_text: String::new(),
+
+            agent_data: AgentData::default(),
         }
     }
 }
@@ -169,33 +183,41 @@ impl eframe::App for STApp {
                 });
 
                 if let Ok(rec) = self.rx.try_recv() {
-                    // self.count += incr;
-                    // println!("recieved {:#?}", rec);
                     self.validation_text = serde_json::from_value(
                         rec.get("data").unwrap().get("token").unwrap().clone(),
                     )
                     .unwrap();
+
+                    write_token(self.validation_text.clone()).unwrap();
                 }
             }
             AppState::Main => {
                 // Update the counter with the async response.
                 if let Ok(rec) = self.rx.try_recv() {
-                    // self.count += incr;
-                    // println!("recieved {:#?}", rec);
-                    self.validation_text = serde_json::from_value(
-                        rec.get("data").unwrap().get("token").unwrap().clone(),
-                    )
-                    .unwrap();
+                    println!("{:#?}", rec);
+                    self.agent_data =
+                        serde_json::from_value(rec.get("data").unwrap().clone()).unwrap();
                 }
 
                 egui::CentralPanel::default().show(ctx, |ui| {
-                    ui.label("Press the button to initiate an HTTP request.");
-                    ui.label("If successful, the count will increase by the following value.");
-
+                    ui.label("You are logged in");
                     ui.add(egui::Slider::new(&mut self.value, 1..=120).text("value"));
+                    ui.label(self.main_text.clone());
+                    ui.label(self.agent_data.account_id.clone());
+                    ui.label("credits");
+                    ui.label(self.agent_data.credits.to_string());
+                    ui.label("current waypoint");
+                    ui.label(self.agent_data.headquarters.clone());
+                    ui.label(self.agent_data.headquarters.clone());
 
-                    if ui.button(format!("Count: {}", self.count)).clicked() {
-                        register_request(self.tx.clone(), ctx.clone());
+                    if ui.button("get data").clicked() {
+                        agent_data_request(self.tx.clone(), ctx.clone(), self.token.clone());
+                        waypoint_request(
+                            self.agent_data.headquarters.clone(),
+                            self.tx.clone(),
+                            ctx.clone(),
+                            self.token.clone(),
+                        )
                     }
                 });
             }
@@ -203,29 +225,8 @@ impl eframe::App for STApp {
     }
 }
 
-// fn send_req(incr: u32, tx: Sender<u32>, ctx: egui::Context) {
-//     tokio::spawn(async move {
-//         // Send a request with an increment value.
-//         let body: HttpbinJson = Client::default()
-//             .post("https://httpbin.org/anything")
-//             .json(&Body { incr })
-//             .send()
-//             .await
-//             .expect("Unable to send request")
-//             .json()
-//             .await
-//             .expect("Unable to parse response");
-
-// // After parsing the response, notify the GUI thread of the increment value.
-// let _ = tx.send(body.json.incr);
-// ctx.request_repaint();
-// });
-// }
-
 fn register_request(tx: Sender<serde_json::Value>, ctx: egui::Context) {
-    // let register_url = "https://api.spacetraders.io/v2/register".to_owned();
-    let register_url =
-        "https://stoplight.io/mocks/spacetraders/spacetraders/96627693/register".to_owned();
+    let register_url = "https://api.spacetraders.io/v2/register".to_owned();
     tokio::spawn(async move {
         let res: serde_json::Value = Client::default()
             .post(register_url)
@@ -244,4 +245,87 @@ fn register_request(tx: Sender<serde_json::Value>, ctx: egui::Context) {
         let _ = tx.send(res);
         ctx.request_repaint();
     });
+}
+
+fn agent_data_request(tx: Sender<serde_json::Value>, ctx: egui::Context, token: String) {
+    let agent_data_url = "https://api.spacetraders.io/v2/my/agent";
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        "Content-Type",
+        HeaderValue::from_str("application/json").unwrap(),
+    );
+    headers.insert(
+        "Authorization",
+        HeaderValue::from_str(("Bearer ".to_owned() + token.as_str()).as_str()).unwrap(),
+    );
+
+    tokio::spawn(async move {
+        let res: serde_json::Value = Client::default()
+            .get(agent_data_url)
+            .headers(headers)
+            .send()
+            .await
+            .expect("agent data request failed")
+            .json()
+            .await
+            .unwrap();
+
+        let _ = tx.send(res);
+        ctx.request_repaint();
+    });
+}
+
+#[derive(Deserialize, Serialize)]
+struct AgentRequest {}
+
+#[derive(Default, Deserialize, Serialize)]
+struct AgentData {
+    #[serde(rename = "accountId")]
+    account_id: String,
+    credits: i64,
+    headquarters: String,
+    #[serde(rename = "startingFaction")]
+    starting_faction: String,
+    symbol: String,
+}
+
+fn waypoint_request(
+    waypoint: String,
+    tx: Sender<serde_json::Value>,
+    ctx: egui::Context,
+    token: String,
+) {
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        "Content-Type",
+        HeaderValue::from_str("application/json").unwrap(),
+    );
+    headers.insert(
+        "Authorization",
+        HeaderValue::from_str(("Bearer ".to_owned() + token.as_str()).as_str()).unwrap(),
+    );
+
+    tokio::spawn(async move {
+        let res: serde_json::Value = Client::default()
+            .get(waypoint_url_builder(&waypoint))
+            .headers(headers)
+            .send()
+            .await
+            .expect("agent data request failed")
+            .json()
+            .await
+            .unwrap();
+
+        let _ = tx.send(res);
+        ctx.request_repaint();
+    });
+}
+
+fn waypoint_url_builder(waypoint: &str) -> String {
+    let parts: Vec<&str> = waypoint.split("-").collect();
+    format!(
+        "https://api.spacetraders.io/v2/systems/{}/waypoints/{}",
+        parts[..2].join("-"),
+        parts.join("-")
+    )
 }
